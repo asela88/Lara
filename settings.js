4601c3782677f41e058ea68b560e9ede883dab17
asela88 <aseladinushan2@gmail.com>
2025-01-23 11:39:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "HnxiRLKK#8iD30dBSVs3L0qDOIX1fpEtPHVaw6tgsV3fdPk14z-0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-STATUES_REPLY: process.env.STATUES_REPLY || "true",
+STATUES_REPLY: process.env.STATUES_REPLY || "false",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
